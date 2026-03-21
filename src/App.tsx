70376a2ec34f45522640c95494f10e938ea997d4
Rudransh <RUDRANASH2K@GMAIL.COM>
2026-03-22 02:47:51 +0530

    fix: add basename for github pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/pdf-to-classy-web">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
